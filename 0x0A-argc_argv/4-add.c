760f7372571b44ddcbda879494bb71ed33ad729c
Temitope Jane Alani <111070862+alanitemmy@users.noreply.github.com>
2022-09-30 14:25:42 +0100

    Update 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main -Entry point
+  *
+  *Description: a program that adds positive numbers
+  *If no number is passed to the program, print 0, followed by a new line
+  *If one of the number contains symbols that are not digits, print Error, 
+  *followed by a new line, and return 1
+  *
+  *@argc: argument count
+  *
+  *@argv: argument vector (arrays of string)
+  *
+  *Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else (argv != i)
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
